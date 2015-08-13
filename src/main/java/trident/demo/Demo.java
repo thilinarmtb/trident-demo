3d6d5a73fe3c2dc52c01cd7b1a8f62db6511666a
Thilina Rathnayake <thilinarmtb@gmail.com>
2015-08-13 12:22:18 +0530

    Add comments

--- src/main/java/trident/demo/Demo.java
+++ src/main/java/trident/demo/Demo.java
@@ -23,14 +23,27 @@ import trident.demo.spouts.RepeatWordsSpout;
 public class Demo {
 
     public static StormTopology buildTopology(StateFactory state) {
+        // A Trident topology operates in a higher level than a Storm topology.
         TridentTopology topology = new TridentTopology();
 
-        TridentState wordCounts = topology.newStream("spout1", new RepeatWordsSpout())
+        // Create a simple new stream using the `RepeatWordsSpout`. In this topology,
+        // we group the words emitted by `RepeatWordsSpout` by the field and store the count in
+        // `StateFactory` passed to the function. In order to aggregate across the batches
+        // We use persistentAggregate function. parallelismHint provides a hint for the
+        // parallelism of the topology.
+        TridentState wordCounts = topology.newStream("wordCount", new RepeatWordsSpout())
                 .parallelismHint(1)
                 .groupBy(new Fields("word"))
                 .persistentAggregate(state, new Count(), new Fields("count"))
                 .parallelismHint(1);
 
+        // DRPC stands for Distributed Remote Procedure Call.
+        // We can issue calls to the following DRPC Stream using a client library.
+        // A DRPC call takes two Strings, function name and function arguments.
+        // Function arguments are available as the field `args`.
+        // In the following DRPC Stream defined with the name "words", we split the words
+        // contained in the `args` and return the count of each word currently stored
+        // in the state.
         topology.newDRPCStream("words")
                 .parallelismHint(1)
                 .each(new Fields("args"), new Split(), new Fields("word"))
@@ -45,24 +58,33 @@ public class Demo {
     }
 
     public static void main(String[] args) throws Exception {
+        // We create the redis StateFactory using the `trident-redis` library.
+        // We use redis to store our state.
         StateFactory redis = RedisState.transactional(new InetSocketAddress("localhost", 6379));
 
+        // Build the topology
         StormTopology topology = buildTopology(redis);
 
+        // `conf` stores the configuration information that needs to be supplied with
+        // the strom topology.
         Config conf = new Config();
         conf.setDebug(false);
         if (args != null && args.length > 0) {
+            // Submit the topology to the cluster.
             conf.setNumWorkers(4);
             StormSubmitter.submitTopologyWithProgressBar(args[0], conf, topology);
         } else {
+            // Run locally if no input arguments are present.
             LocalCluster cluster = new LocalCluster();
             cluster.submitTopology("tester", conf, topology);
         }
 
+        // Create a DRPC client
         DRPCClient client = new DRPCClient("localhost", 3772);
 
         try {
             while (true) {
+                // We keep querying the state stored in redis forever.
                 System.out.println("DRPC: " + client.execute("words", "apple ball cat dog"));
                 Utils.sleep(2000);
             }
--- src/main/java/trident/demo/spouts/RWBatchCoordinator.java
+++ src/main/java/trident/demo/spouts/RWBatchCoordinator.java
@@ -12,13 +12,30 @@ public class RWBatchCoordinator implements ITridentSpout.BatchCoordinator<List<L
     private static final long serialVersionUID = 1L;
     private static final Logger LOG = LoggerFactory.getLogger(RWBatchCoordinator.class);
 
+    // `initializeTransaction` method is responsible for creating metadata for a transaction.
+    // This metadata should be capable of replaying the exact batch in case of a failure (since
+    // we are using a transient spout). Created metadata is stored in zookeeper and used
+    // when replaying the batches.
+    //
+    // `currMetaData` is the metadata for the current batch and `prevMetaData` is the metadata for the
+    // previous batch. We need to update and return `currMetaData` based on `prevMetaData` in our
+    // demonstration.
+    //
+    // In our demonstration we store a count and the mod of the transaction id by 2. The stored mod
+    // value helps to select the right batch to replay and the stored count helps to decide when to
+    // stop emitting the batches. Read more about this function and other interface functions in the following link:
+    // https://github.com/apache/storm/blob/master/storm-core/src/jvm/storm/trident/spout/ITridentSpout.java#L29
     @Override
     public List<Long> initializeTransaction(final long txid, final List<Long> prevMetaData, List<Long> currMetaData) {
         LOG.info("Initializing transaction [" + txid + "]");
 
         if (prevMetaData == null && currMetaData == null) {
+            // This is the very first batch, we initilize it with a count 0 and the mod
+            // value of the transaction
             currMetaData = new ArrayList<Long>() {{add(0L); add(txid % 2);}};
         } else if (currMetaData == null) {
+            // This is the first time the batch with transaction id `txid` is going to be
+            // emitted. We update `currMetaData` based on previous metadata.
             currMetaData = new ArrayList<Long>() {{add(prevMetaData.get(0) + 1); add(txid % 2);}};
         }
         return currMetaData;
